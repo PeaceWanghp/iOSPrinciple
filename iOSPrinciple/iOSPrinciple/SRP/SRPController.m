45a04cfecd002b7ca679bfbdf337fcb338b20778
wangheping <wangheping@b.360.cn>
2020-11-03 22:19:13 +0800

    add single responsibility principle

--- iOSPrinciple/iOSPrinciple/SRP/SRPController.m
+++ iOSPrinciple/iOSPrinciple/SRP/SRPController.m
@@ -16,6 +16,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self.model appendOpenedHeader:@"定义:"];
+    [self.model appendDarkItemWithTitle:@"Single Responsibility Principle\n(单一职责原则):"
+                                  class:[UIViewController class]];
+    [self.model appendDarkItemWithTitle:@"一个类只负责一组相关的事情，\n对应到代码中：一个类有多个方法，这些方法是相关的。"
+                                  class:[UIViewController class]];
+    
+    [self.model appendOpenedHeader:@"适用范围:"];
+    [self.model appendDarkItemWithTitle:@"1.指导类的设计。"
+                                  class:[UIViewController class]];
+    [self.model appendDarkItemWithTitle:@"2.只适用于基础类，而不适合于基于基础类构建的复杂的聚合类。"
+                                  class:[UIViewController class]];
 }
 
 @end
--- iOSPrinciple/iOSPrinciple/ViewController.m
+++ iOSPrinciple/iOSPrinciple/ViewController.m
@@ -19,9 +19,23 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    [self.model appendOpenedHeader:@"高内聚"];
+    [self.model appendOpenedHeader:@"Cohesion(高内聚):"];
+    [self.model appendDarkItemWithTitle:@"Coincidental cohesion\n(偶然内聚)" class:[UIViewController class]];
+    [self.model appendDarkItemWithTitle:@"Logical cohesion\n(逻辑内聚)" class:[UIViewController class]];
+    [self.model appendDarkItemWithTitle:@"Procedural cohesion\n(过程内聚)" class:[UIViewController class]];
+    [self.model appendDarkItemWithTitle:@"Informational/Communicational cohesion\n(信息内聚)" class:[UIViewController class]];
+    [self.model appendDarkItemWithTitle:@"Sequential cohesion\n(顺序内聚)" class:[UIViewController class]];
+    [self.model appendDarkItemWithTitle:@"Functional cohesion\n(功能内聚)" class:[UIViewController class]];
     
-    [self.model appendOpenedHeader:@"低耦合"];
+    [self.model appendOpenedHeader:@"Coupling(低耦合):"];
+    [self.model appendDarkItemWithTitle:@"No coupling\n(无耦合)" class:[UIViewController class]];
+    [self.model appendDarkItemWithTitle:@"Message coupling\n(消息耦合)" class:[UIViewController class]];
+    [self.model appendDarkItemWithTitle:@"Data coupling\n(数据耦合)" class:[UIViewController class]];
+    [self.model appendDarkItemWithTitle:@"Data structured coupling\n(数据结构耦合)" class:[UIViewController class]];
+    [self.model appendDarkItemWithTitle:@"Control coupling\n(控制耦合)" class:[UIViewController class]];
+    [self.model appendDarkItemWithTitle:@"External coupling\n(外部耦合)" class:[UIViewController class]];
+    [self.model appendDarkItemWithTitle:@"Globaling coupling\n(全局耦合)" class:[UIViewController class]];
+    [self.model appendDarkItemWithTitle:@"Content coupling\n(内容耦合)" class:[UIViewController class]];
     
     [self.model appendOpenedHeader:@"Principle(设计原则):"];
     [self.model appendDarkItemWithTitle:@"Single Responsibility Principle\n(单一职责原则)" class:[SRPController class]];
