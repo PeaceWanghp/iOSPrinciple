70e6a6d53c5db905cb5f15071b038f342cff0c08
wangheping <wangheping@b.360.cn>
2020-11-06 22:08:30 +0800

    change list

--- iOSPrinciple/iOSPrinciple/ViewController.m
+++ iOSPrinciple/iOSPrinciple/ViewController.m
@@ -31,13 +31,13 @@
     [self.model appendOpenedHeader:@"Principle(设计原则):"];
     [self.model appendDarkItemWithTitle:@"Open Close Principle\n开闭原则：总的指导思想"
                                   class:[OCPController class]];
-    [self.model appendItemWithTitle:@"Single Responsibility Principle\n单一职责原则：指导类设计"
-                                  class:[SRPController class]];
-    [self.model appendItemWithTitle:@"Law Of Demeter\n迪米特法则/最少知道原则：指导类之间耦合设计(高内聚，低耦合)"
+    [self.model appendDarkItemWithTitle:@"Law Of Demeter\n迪米特法则/最少知道原则：总的指导思想(高内聚，低耦合)"
                                   class:[LODController class]];
+    [self.model appendDarkItemWithTitle:@"Single Responsibility Principle\n单一职责原则：指导类设计"
+                                  class:[SRPController class]];
     [self.model appendDarkItemWithTitle:@"Dependncy Inversion Principle\n依赖反转原则：指导如何抽象"
                                   class:[DIPController class]];
-    [self.model appendDarkItemWithTitle:@"Composite Reuse Principle\n合成复用原则"
+    [self.model appendDarkItemWithTitle:@"Composite Reuse Principle\n合成复用原则：指导选择“继承复用”或“合成复用”"
                                   class:[CRPController class]];
     [self.model appendDarkItemWithTitle:@"Liskow Substitution Principle\n里氏替换原则：指导类继承设计(多态)"
                                   class:[LSPController class]];
@@ -45,8 +45,8 @@
                                   class:[ISPController class]];
     
     [self.model appendOpenedHeader:@"Cohesion/Coupling(内聚/耦合):"];
-    [self.model appendItemWithTitle:@"Cohesion(内聚):低---->高" class:[CohesionController class]];
-    [self.model appendItemWithTitle:@"Coupling(耦合):低---->高" class:[CouplingController class]];
+    [self.model appendDarkItemWithTitle:@"Cohesion(内聚):低---->高（6级）" class:[CohesionController class]];
+    [self.model appendDarkItemWithTitle:@"Coupling(耦合):低---->高（8级）" class:[CouplingController class]];
 }
 
 @end
